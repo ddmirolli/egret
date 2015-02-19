37614e182efa77a5d4b2899b23ad0b34dca6c3e2
Daniel Mirolli <ddmirolli@gmail.com>
2015-02-19 01:51:52 -0600

    Set up Devise and then lost server to constant app_path

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module Egret
   class Application < Rails::Application
@@ -21,6 +21,8 @@ module Egret
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    
+    # Required for Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
